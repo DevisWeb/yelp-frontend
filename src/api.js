7bb391ba99f0f82ee6a7bebe12d4220aa2b9e996
Elie <eliegebransoued@gmail.com>
2021-01-29 13:10:57 +0100

    Best Commit Message Ever !!! (With Steph) ----------------------------------------- 1- GET AllRestaurant Request (Axios) done
    That´s all folks!
    Just wanted to show off

--- src/api.js
+++ src/api.js
@@ -1,1 +1,14 @@
 import axios from "axios";
+
+const getRestaurants = `${process.env.REACT_APP_API_ENDPOINT}`;
+
+const Api = {
+  restaurants: {
+    // getSearchResults: (search) => axios.get(`${getRestaurants}&query=${search}`),
+    getAll: () => axios.get(`${getRestaurants}`),
+    // getById: (id) => axios.get(`${urlPost}&sys.id=${id}`),
+    // getByUser: (userId) => a,
+  },
+};
+
+export default Api;
--- src/views/viewrestaurantsall.js
+++ src/views/viewrestaurantsall.js
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+import Api from "../api.js";
+import axios from "axios";
 export default function ViewRestaurantsAll() {
-  return "ViewRestaurantsAll";
+  const [allRestaurants, setAllRestaurants] = useState([]);
+
+  // if (search) {
+  //   Api.restaurant
+  //     // .getSearchResults(search)
+  //     // .then((response) => setPostsAll(response.data.items))
+  //     // .catch((error) => console.error(error));
+
+  useEffect(() => {
+    axios
+      .get("https://mini-yelp-db.herokuapp.com/restaurants")
+      .then((response) => {
+        setAllRestaurants(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log("There was an error with your request");
+      });
+  }, []);
+
+  return <div></div>;
 }
+
+// {allRestaurants.map((restaurant, index) => {
+//   return (
+//     <div key={index}>{console.log(`restaurant : ${restaurant}`)}</div>
+//   );
+// })
